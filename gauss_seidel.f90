e8c6f4208c67d09d53c63acb1d35952d6e482063
Gianluca Seaford <Luca.Seaford@warwick.ac.uk>
2024-11-29 13:58:26 +0000

    Add Key Modules
    Added the base modules for the code and a framework for the Gauss-Seidel module.

---
+++ gauss_seidel.f90
@@ -0,0 +1,9 @@
+MODULE solve_gauss_seidel
+    !> Solves the Poisson equation iteratively via the Gauss-Seidel method.
+    !! author: Gianluca Seaford
+    !! version: 1.0
+    !! date: 2024-11-29
+    
+    IMPLICIT NONE
+
+END MODULE solve_gauss_seidel
