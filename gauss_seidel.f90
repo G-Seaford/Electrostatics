c74af55272059a60ca9edc19d05a9792774ba2e2
Gianluca Seaford <Luca.Seaford@warwick.ac.uk>
2025-01-06 14:09:39 +0000

    0/01/25 14:09

--- gauss_seidel.f90
+++ gauss_seidel.f90
@@ -28,5 +28,53 @@ MODULE solve_gauss_seidel
 
     CONTAINS
 
+    SUBROUTINE InitialiseGaussSeidelData(data, init_type, nx, ny, dx, dy, status)
+        !> Initialises the data structure for the Gauss-Seidel method.
+        !! @param data: The GaussSeidelData structure to initialise.
+        !! @param nx: The number of grid points in the x-direction.
+        !! @param ny: The number of grid points in the y-direction.
+        !! @param dx: The grid spacing in the x-direction.
+        !! @param dy: The grid spacing in the y-direction.
+
+        TYPE(GaussSeidelData), INTENT(INOUT) :: data
+        CHARACTER(LEN=*), INTENT(IN) :: init_type
+        INTEGER, INTENT(IN) :: nx, ny
+        REAL, INTENT(IN) :: dx, dy
+        INTEGER, INTENT(INOUT) :: status
+
+        data%nx = nx
+        data%ny = ny
+
+        data%dx = dx
+        data%dy = dy
+
+        ALLOCATE(data%phi(0:nx+1, 0:ny+1), STAT=status)
+        IF (status /= 0) THEN
+            PRINT *, "Error allocating memory for phi"
+            RETURN
+        END IF
+
+        ALLOCATE(data%rho(0:nx+1, 0:ny+1), STAT=status)
+        IF (status /= 0) THEN
+            PRINT *, "Error allocating memory for rho"
+            RETURN
+        END IF
+
+        SELECT CASE(init_type)
+
+            CASE("null")
+                !! Initialise phi and rho to zero
+                data%phi = 0.0
+                data%rho = 0.0
+
+            CASE("single")
+
+            CASE("double")
+        
+        END SELECT
+
+        
+
+    END SUBROUTINE InitialiseGaussSeidelData
 
 END MODULE solve_gauss_seidel
