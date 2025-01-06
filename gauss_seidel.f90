7d2b3931e266788322f965041a700bf55d02e6c2
Gianluca Seaford <Luca.Seaford@warwick.ac.uk>
2025-01-06 10:04:01 +0000

    06/01/25 10:03
    Added a rough outline for a derived type to store the Gauss-Seidel data

--- gauss_seidel.f90
+++ gauss_seidel.f90
@@ -6,4 +6,27 @@ MODULE solve_gauss_seidel
     
     IMPLICIT NONE
 
+    !> Define derived type for data storage
+    TYPE :: GaussSeidelData
+        INTEGER :: nx, ny
+        REAL :: dx, dy
+
+        !! Poisson equation data
+        REAL, DIMENSION(:,:), ALLOCATABLE :: phi
+        REAL, DIMENSION(:,:), ALLOCATABLE :: rho
+
+        !! Electric field data
+        REAL, DIMENSION(:), ALLOCATABLE :: Ex
+        REAL, DIMENSION(:), ALLOCATABLE :: Ey
+
+        !! Electron data
+        REAL, DIMENSION(:), ALLOCATABLE :: position_x, position_y
+        REAL, DIMENSION(:), ALLOCATABLE :: velocity_x, velocity_y
+        REAL, DIMENSION(:), ALLOCATABLE :: acceleration_x, acceleration_y
+
+    END TYPE GaussSeidelData
+
+    CONTAINS
+
+
 END MODULE solve_gauss_seidel
