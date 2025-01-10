bff35d566c5006433aef4b43144e9609b5e9e843
FacundoJCosta <facundojosecosta@hotmail.com>
2025-01-10 16:57:30 +0000

    Update Program.f90

--- Program.f90
+++ Program.f90
@@ -4,6 +4,7 @@ PROGRAM main
   USE ISO_FORTRAN_ENV
   USE run_data_module
   USE write_netcdf
+  USE command_line
   IMPLICIT NONE
 
   TYPE(run_data) :: run
@@ -14,8 +15,10 @@ PROGRAM main
   REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: position
   REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: velocity
   REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: acceleration
-  INTEGER :: num_steps
+  REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: rho
 
+  INTEGER :: num_steps
+  INTEGER :: i,j
   INTEGER :: nx
   INTEGER :: ny
   REAL(REAL64), DIMENSION(2) :: axis_range_x
@@ -26,6 +29,7 @@ PROGRAM main
   REAL(REAL64) ::dy
   REAL(REAL64) ::delta_t
   REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: potential
+  REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: out_potential
 
   num_steps = 1000
   delta_t = 0.01
@@ -47,21 +51,23 @@ PROGRAM main
   ! here we have the grid created
 
   ALLOCATE(potential(0:nx+1, 0:ny+1))
+  ALLOCATE(out_potential(1:nx, 1:ny))
   ALLOCATE(position(2,num_steps))
   ALLOCATE(velocity(2,num_steps))
   ALLOCATE(acceleration(2,num_steps))
+  ALLOCATE(rho(1:nx, 1:ny))
 
 
 
 
   position(1,1) = 0.0_REAL64
   position(2,1) = 0.0_REAL64
-  velocity(1,1) = -0.01_REAL64
+  velocity(1,1) = -0.001_REAL64
   velocity(2,1) = 0.002_REAL64
   !Then run gauss siedel here to get what the potentail should be!
 
   potential = 1.0_REAL64
-
+  rho = 6.0_REAL64
 
 
   !Then run create E_field from the potentail
@@ -77,6 +83,16 @@ PROGRAM main
   
   ! USE NETcdf now so that we can ouput all the figures nicely
 
+
+  ! need to reduce the potentail as needs to be 1,nx on output
+
+  do i = 1, nx, 1
+    do j = 1,ny, 1
+      out_potential(i,j) = potential(i,j)
+    end do
+  end do
+
+
   run%axis_x = axis_x
   run%axis_y = axis_y
   run%E_x = E_x
@@ -84,18 +100,20 @@ PROGRAM main
   run%position = position
   run%velocity = velocity
   run%acceleration = acceleration
-  run%potential = potential
+  run%potential = out_potential  ! remove the guard cells
   run%num_steps = num_steps
   run%nx = nx
   run%ny = ny
-  
+  run%rho = rho
+
 
   CALL writer(run,"saved.nc", err)
   if(err /= 0)then
     print*, "Error writing to the file"
   end if
 
-
+  DEALLOCATE(out_potential)
+  DEALLOCATE(rho)
   DEALLOCATE(position)
   DEALLOCATE(velocity)
   DEALLOCATE(acceleration)
